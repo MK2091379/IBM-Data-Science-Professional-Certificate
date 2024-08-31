8d0b1a2800086d4c61173ae9ed7d73270756a7e4
MK2091379 <m.kazemi2091379@gmail.com>
2024-08-31 18:31:57 +0330

    lab8 was completed

--- 08-Data Visualization with Python/08-Dash_Interactivity_4.7.py
+++ 08-Data Visualization with Python/08-Dash_Interactivity_4.7.py
@@ -1,53 +1,40 @@
-# Import required libraries
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
-import plotly.graph_objects as go
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
-# Read the airline data into pandas dataframe
-airline_data =  pd.read_csv('httpscf-courses-data.s3.us.cloud-object-storage.appdomain.cloudIBMDeveloperSkillsNetwork-DV0101EN-SkillsNetworkData%20Filesairline_data.csv', 
-                            encoding = ISO-8859-1,
-                            dtype={'Div1Airport' str, 'Div1TailNum' str, 
-                                   'Div2Airport' str, 'Div2TailNum' str})
 
-# Create a dash application
-app = dash.Dash(__name__)
+airline_data = pd.read_csv('./airline_data.csv', 
+                            encoding="ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
 
-# Get the layout of the application and adjust it.
-# Create an outer division using html.Div and add title to the dashboard using html.H1 component
-# Add a html.Div and core input text component
-# Finally, add graph component.
-app.layout = html.Div(children=[html.H1('Airline Performance Dashboard', style={'textAlign''center',
-                                                                                'color' '#503D36',
-                                                                                'font-size'40}),
-                                html.Div([Input Year, dcc.Input(id='input-year', value='2010',
-                                                                type='number', style={'height''50px',
-                                                                                      'font-size'35}
-                                                                                      ),], 
-                                style={'font-size'40}),
+
+app = Dash(__name__)
+
+
+app.layout = html.Div(children=[html.H1(children='Airline Dashboard',
+                                        style={'textAlign': 'center',
+                                               'color': '#503D36',
+                                               'fontSize': '40px'}),
+                                html.Div(['Input:', dcc.Input(id='input-year', value='2000', type='number', 
+                                                              style={'height':'50px', 'font-size': 35})], 
+                                         style={'fontSize': '40px'}),
                                 html.Br(),
                                 html.Br(),
-                                html.Div(dcc.Graph(id='line-plot')),
-                                ])
-# add callback decorator
-@app.callback(Output(component_id='line-plot', component_property='figure'),
-               Input(component_id='input-year', component_property='value'))
+                                html.Div(
+                                    dcc.Graph(id='line-plot')),
+                               ], style={'backgroundColor':'#111111'})
 
-
-# Add computation to callback function and return graph
-def get_graph(entered_year)
-    # Select 2019 data
-    df =  airline_data[airline_data['Year']==int(entered_year)]
-    
-    # Group the data by Month and compute average over arrival delay time.
-    line_data = df.groupby('Month')['ArrDelay'].mean().reset_index()
-
-    fig = go.Figure(data=go.Scatter(x=line_data['Month'], y=line_data['ArrDelay'], mode='lines', marker=dict(color='green')))
-    fig.update_layout(title='Month vs Average Flight Delay Time', xaxis_title='Month', yaxis_title='ArrDelay')
+@app.callback(
+    Output(component_id='line-plot', component_property='figure'),
+    Input(component_id='input-year', component_property='value'))
+def create_figure(year):
+    df = airline_data[airline_data['Year'] == int(year)][['Month', 'ArrDelay']]
+    df.rename(columns={'ArrDelay': 'ArrDelayAvg'}, inplace=True)
+    mean_delays = df.groupby('Month').mean().reset_index()
+    fig = px.line(mean_delays, x='Month', y='ArrDelayAvg', title='Sales Over Years')
     return fig
-
-# Run the app
-if __name__ == '__main__'
-    app.run_server()
+                               
+if __name__ == '__main__':
+    app.run()
