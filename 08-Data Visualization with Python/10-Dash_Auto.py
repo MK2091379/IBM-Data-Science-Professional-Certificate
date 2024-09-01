d91ffb6a17a2d5530d35c386df558aa930db6e46
MK2091379 <m.kazemi2091379@gmail.com>
2024-09-01 15:19:49 +0330

    lab10 was completed

--- 08-Data Visualization with Python/10-Dash_Auto.py
+++ 08-Data Visualization with Python/10-Dash_Auto.py
@@ -2,84 +2,59 @@ import pandas as pd
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
-import plotly.graph_objects as go
+from dash.dependencies import Input, Output
 import plotly.express as px
-from dash import no_update
 
 app = dash.Dash(__name__)
 
-# REVIEW1: Clear the layout and do not display exception till callback gets executed
-app.config.suppress_callback_exceptions = True
-
 # Read the automobiles data into pandas dataframe
-auto_data =  pd.read_csv('automobileEDA.csv', 
-                            encoding = "ISO-8859-1",
-                            )
+auto_data = pd.read_csv('automobileEDA.csv', encoding="ISO-8859-1")
 
-#Layout Section of Dash
-
-app.layout = html.Div(children=[html.H1('Car Automobile Components', 
-                                style={'textAlign': 'center', 'color': '#503D36',
-                                'font-size': 24}),
-
-     #outer division starts
-     html.Div([
-                   # First inner divsion for  adding dropdown helper text for Selected Drive wheels
-                    html.Div(
-                            html.H2('Drive Wheels Type:', style={'margin-right': '2em'}),
-                     ),
-
-
-                    dcc.Dropdown(
-                            id='demo-dropdown', 
-                            options=[
-                                    {'label': 'Rear Wheel Drive', 'value': 'rwd'},
-                                    {'label': 'Front Wheel Drive', 'value': 'fwd'},
-                                    {'label': 'Four Wheel Drive', 'value': '4wd'},
-                                    ],
-                            value='rwd'
-
-                    ),
-
-                    #Second Inner division for adding 2 inner divisions for 2 output graphs 
-                    html.Div([
-
-                        html.Div([ ], id='plot1'),
-                        html.Div([ ], id='plot2')
-
-                    ], style={'display': 'flex'}),
-
-
+# Layout Section of Dash
+app.layout = html.Div(children=[
+    html.H1('Car Automobile Components', style={'textAlign': 'center', 'color': '#503D36', 'font-size': 24}),
+    
+    html.Div([
+        html.Div(html.H2('Drive Wheels Type:', style={'margin-right': '2em'})),
+        
+        dcc.Dropdown(
+            id='demo-dropdown',
+            options=[
+                {'label': 'Rear Wheel Drive', 'value': 'rwd'},
+                {'label': 'Front Wheel Drive', 'value': 'fwd'},
+                {'label': 'Four Wheel Drive', 'value': '4wd'},
+            ],
+            value='rwd'
+        ),
+        
+        html.Div([
+            html.Div(dcc.Graph(id='plot1')),
+            html.Div(dcc.Graph(id='plot2'))
+        ], style={'display': 'flex'}),
     ])
-    #outer division ends
-
 ])
-#layout ends
 
-#Place to add @app.callback Decorator
-@app.callback([Output(component_id='plot1', component_property='children'),
-               Output(component_id='plot2', component_property='children')],
-               Input(component_id='demo-dropdown', component_property='value'))
-
-#Place to define the callback function .
-#TASK 3F
+# Callback to update charts based on dropdown value
+@app.callback(
+    [Output(component_id='plot1', component_property='figure'),
+     Output(component_id='plot2', component_property='figure')],
+    Input(component_id='demo-dropdown', component_property='value')
+)
 def display_selected_drive_charts(value):
-
-
-
-   filtered_df = auto_data[auto_data['drive-wheels']==value].groupby(['drive-wheels','body-style'],as_index=False). \
-            mean()
-
-   filtered_df = filtered_df
-
-   fig1 = px.pie(filtered_df, values='price', names='body-style', title="Pie Chart")
-   fig2 = px.bar(filtered_df, x='body-style', y='price', title='Bar Chart')
-
-   return [dcc.Graph(figure=fig1),
-            dcc.Graph(figure=fig2) ]
-
-
+    # Filter the DataFrame based on the selected drive wheels
+    filtered_df = auto_data[auto_data['drive-wheels'] == value]
+    
+    # Handle NaN values in the 'price' column
+    filtered_df = filtered_df[filtered_df['price'].notna()]
+    
+    # Group by 'drive-wheels' and 'body-style', calculating the mean price
+    grouped_df = filtered_df.groupby(['drive-wheels', 'body-style'], as_index=False)['price'].mean().reset_index()
+    
+    # Create figures
+    fig1 = px.pie(grouped_df, values='price', names='body-style', title="Pie Chart")
+    fig2 = px.bar(grouped_df, x='body-style', y='price', title='Bar Chart')
+    
+    return fig1, fig2
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
